31b353d93c8d009b203618a97c7681c0fe401ee7
Jim Palowski <palowskijim@gmail.com>
2018-10-30 16:20:41 -0700

    removed 1 book from each category

---
+++ src/BookShelf.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class BookShelf extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello
+            </div>
+        )
+    }    
+}
+
+export default BookShelf
