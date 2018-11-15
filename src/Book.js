4a6b94ebd1f3f0b060d88bfd743303e67e473635
Jim Palowski <palowskijim@gmail.com>
2018-11-14 17:41:13 -0800

    Add handleChange

--- src/Book.js
+++ src/Book.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 class Book extends React.Component {
-    constructor(props){
-    super(props);
-    this.state = {
-        newBook:{
-            read:'',
-            currentlyReading:''
-        }
-    }
-    }    
-    updateState(newBook) {
-        this.setState({
-            value: newBook.value
-        });
-    }
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            value : ''
+        };
+    
+        this.handleChange = this.handleChange.bind(this);
+    }  
 
+    handleChange(event) {
+        this.setState({ 
+            value: event.target.value 
+        })
+        console.log(this.state.value)
+    }
     render() {
 
         return (
@@ -23,7 +24,7 @@ class Book extends React.Component {
                 <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
                         <div className="book-shelf-changer">
-                            <select onChange={this.updateState}>
+                            <select onChange={this.handleChange}>
                                 <option value="move" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
