1608fd2f2fd65de3a04095af1a1182e604035995
Jim Palowski <palowskijim@gmail.com>
2018-11-15 18:59:43 -0800

    Moved Book form Bookshelf to Books

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -13,36 +13,15 @@ class BookShelf extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
-                <div className="bookshelf">
+                <div className="bookshelfCurrent">
                   <h2 className="bookshelf-title">Currently Reading</h2>
                     <Books passingBooks={this.props.passedBooks} />
                 </div>
-                <div className="bookshelf">
+                <div className="bookshelfWant">
                   <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">1776</div>
-                          <div className="book-authors">David McCullough</div>
-                        </div>
-                      </li>
-                    </ol>
-                  </div>
+                    <Books />
                 </div>
-                <div className="bookshelf">
+                <div className="bookshelfRead">
                   <h2 className="bookshelf-title">Read</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
